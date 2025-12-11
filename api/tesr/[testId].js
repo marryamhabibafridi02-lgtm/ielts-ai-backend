eade6532409ab956e73fd067044b9bda111403c1
marryamhabibafridi02-lgtm <marryamhabibafridi02@gmail.com>
2025-12-11 03:18:10 -0700

    Delete api directory

--- api/tesr/[testId].js
+++
@@ -1,9 +0,0 @@
-// api/test/[testId].js
-const TESTS = global.TESTS ||= {};
-
-export default function handler(req, res) {
-  const { testId } = req.query;
-  const t = TESTS[testId];
-  if (!t) return res.status(404).json({ error: "test not found" });
-  return res.json({ ok: true, test: t });
-}
